2f3a7138eba70ecbefbb50d04dc5ba751683e6a2
Alberto <albertoglezb@hotmail.com>
2024-06-21 17:01:10 -0600

    The first attempt to get the rows was added.

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -10,8 +10,15 @@ test("Render one row per user", () => {
   render(<UserList users={users} />);
 
   //Find all the rows in the table
+  /*Genera una URL con el código generado para nuestro componente
+  al cual podemos navegar.
+  Despliega la URL en la consola cuando se ejecuta la prueba.
+   */
+  // screen.logTestingPlaygroundURL();
+  const rows = screen.getAllByRole("row");
 
   //Assertion: correct number of rows in the table
+  expect(rows).toHaveLength(2);
 });
 
 test("Render the email and name of each user", () => {
