83f2a5a67c360146c5311036abdae1ca53f0edd0
Alberto <albertoglezb@hotmail.com>
2024-06-21 21:15:22 -0600

    A failing test was created. Small intro to TDD.

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -43,3 +43,21 @@ test("It calls onUserAdd when the form is submitted", () => {
   expect(mock).toHaveBeenCalled();
   expect(mock).toHaveBeenCalledWith({ name: "jane", email: "jane@jane.com" });
 });
+
+test("empties the two inputs when form is sumitted", () => {
+  render(<UserForm onUserAdd={() => {}} />);
+
+  const nameInput = screen.getByRole("textbox", { name: /name/i });
+  const emailInput = screen.getByRole("textbox", { name: /email/i });
+  const button = screen.getByRole("button");
+
+  user.click(nameInput);
+  user.keyboard("jane");
+  user.click(emailInput);
+  user.keyboard("jane@jane.com");
+
+  user.click(button);
+
+  expect(nameInput).toHaveValue("");
+  expect(emailInput).toHaveValue("");
+});
